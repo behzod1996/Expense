2c7ffa70c344548f23410d14d7b20690d7d44651
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-27 13:33:51 +0900

    Add database builder to get the database instance to TransactionDatabase.kt

--- app/src/main/java/com/example/expense/data/local/TransactionDatabase.kt
+++ app/src/main/java/com/example/expense/data/local/TransactionDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.expense.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.expense.data.local.dao.TransactionDao
 import com.example.expense.data.local.entity.Transaction
@@ -10,9 +12,31 @@ import com.example.expense.data.local.entity.Transaction
     version = 1,
     exportSchema = false
 )
-abstract class TransactionDatabase: RoomDatabase() {
+abstract class TransactionDatabase : RoomDatabase() {
 
-    abstract fun getDao() : TransactionDao
+    abstract fun getDao(): TransactionDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: TransactionDatabase? = null
 
+        fun getTransactionDatabase(context: Context): TransactionDatabase {
+
+            val tempInstance = INSTANCE
+            if (tempInstance != null) return tempInstance
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TransactionDatabase::class.java,
+                    "transaction_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
