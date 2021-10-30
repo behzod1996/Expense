08cd01a54994080c21bc0038c4a7a9bbac5d70a0
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-30 18:11:29 +0900

    Update TransactionRepositoryImpl.kt

--- app/src/main/java/com/example/expense/data/repository/TransactionRepositoryImpl.kt
+++ app/src/main/java/com/example/expense/data/repository/TransactionRepositoryImpl.kt
@@ -1,18 +1,19 @@
 package com.example.expense.data.repository
 
 import com.example.expense.data.local.TransactionDatabase
+import com.example.expense.data.local.dao.TransactionDao
 import com.example.expense.data.local.entity.Transaction
 import kotlinx.coroutines.flow.Flow
 
-class TransactionRepositoryImpl(private val database: TransactionDatabase) : TransactionRepository {
-    override suspend fun insert(transaction: Transaction) = database.getDao().insert(transaction)
+class TransactionRepositoryImpl(private val dao: TransactionDao) : TransactionRepository {
+    override suspend fun insert(transaction: Transaction) = dao.insert(transaction)
 
-    override suspend fun update(transaction: Transaction) = database.getDao().update(transaction)
+    override suspend fun update(transaction: Transaction) = dao.update(transaction)
 
-    override suspend fun delete(transaction: Transaction) = database.getDao().delete(transaction)
+    override suspend fun delete(transaction: Transaction) = dao.delete(transaction)
 
-    override fun getAllTransaction(): Flow<List<Transaction>> =
-        database.getDao().getAllTransaction()
+    override fun getAllTransaction(): Flow<List<Transaction>> = dao.getAllTransaction()
 
-    override fun getById(id: Int): Flow<Transaction> = database.getDao().getTransactionById(id)
+    override fun getById(id: Int): Flow<Transaction> = dao.getTransactionById(id)
 }
+
